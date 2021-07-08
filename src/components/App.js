dca7c7df2dfcd0f2c21c98419710237892f86e6a
Pwako11 <akolo@hotmail.com>
2021-07-08 13:46:00 -0400

    AAQ push

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  handleChangeFilterType = e =>{
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: e
+      }
+    })
+  }
+
+  findPets = () => {
+    let url = "/api/pets"
+    let pet = this.state.filters.type
+    let petUrl = url + `?type=${pet}` 
+   
+    console.log("first url", url)
+    if(this.state.filters.type !== 'all'){
+      url = petUrl
+    }
+
+    console.log("second url", url)
+    fetch(url)
+    .then(res => res.json())
+    .then(pets => this.setState({pets}))
+  }
+
+  handleAdoptPet = petId => {
+    console.log("From App.js handleAdoptedPet", petId)
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, petId]
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters filters={this.state.filters} onChangeType={this.handleChangeFilterType} onFindPetsClick={this.findPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} adoptedPets={this.state.adoptedPets} onAdoptPet={this.handleAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    const pets = this.props.pets.map(pet =>{
+      <Pet pet={pet} key={pet.id} onAdoptPet= {this.props.onAdoptPet} isAdopted= {this.props.adoptedPets.includes(pet.id)} />
+    })
+
+    return( 
+      <div className="ui cards">
+        {pets}
+      </div>
+    )
   }
 }
 
